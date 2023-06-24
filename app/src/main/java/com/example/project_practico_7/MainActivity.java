eb9bcfa954bdeb346b4c8280d279c6af27494290
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-24 12:10:12 -0400

    Se agrega función a botón Abrir Calendario.

--- app/src/main/java/com/example/project_practico_7/MainActivity.java
+++ app/src/main/java/com/example/project_practico_7/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.project_practico_7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textFecha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textFecha = findViewById(R.id.textViewFecha);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button botonCalendario = findViewById(R.id.buttonCalendar);
+        botonCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCalendario();
+            }
+        });
+    }
+
+    public void abrirCalendario(){
+        Calendar calendario = Calendar.getInstance();
+        int year = calendario.get(Calendar.YEAR);
+        int month = calendario.get(Calendar.MONTH);
+        int day = calendario.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String fecha = dayOfMonth + "/" + month + "/" + year;
+                textFecha.setText(fecha);
+            }
+        },year,month,day);
+        datePicker.show();
+
     }
 }
