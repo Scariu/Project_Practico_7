413b35f3684d24c2f4f1ca50f805ec211bcf88b6
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-25 03:57:02 -0400

    Se agrega botón Google. Pendiente arreglar nightmode.

--- app/src/main/java/com/example/project_practico_7/MainActivity.java
+++ app/src/main/java/com/example/project_practico_7/MainActivity.java
@@ -4,28 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textFecha;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textFecha = findViewById(R.id.textViewFecha);
         initListeners();
     }
 
     private void initListeners() {
         Button botonCalendario = findViewById(R.id.buttonCalendar);
-        Switch switchDayNight = findViewById(R.id.switchDayNight);
+        Switch switchNight = findViewById(R.id.switchNight);
+
         botonCalendario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,19 +37,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        switchDayNight.setOnClickListener(new View.OnClickListener() {
+        switchNight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (switchDayNight.isChecked()) {
+                if (switchNight.isChecked()) {
                     setDayNight(0);
+                    Toast.makeText(getBaseContext(), "Enable Dark Mode", Toast.LENGTH_SHORT).show();
                 } else {
                     setDayNight(1);
+                    Toast.makeText(getBaseContext(), "Enable Light Mode", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
     }
 
     public void abrirCalendario(){
+        TextView textFecha = findViewById(R.id.textViewFecha);
         Calendar calendario = Calendar.getInstance();
         int year = calendario.get(Calendar.YEAR);
         int month = calendario.get(Calendar.MONTH);
